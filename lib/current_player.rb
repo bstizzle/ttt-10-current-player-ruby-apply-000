c5b1d283d6d74c4c26115c32d56d78001e2032f6
Benjamin Stein <whereyouben@gmail.com>
2020-09-13 18:44:54 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+
+def turn_count(board)
+  #establish counters
+  turnCounter = 0
+  occupiedSpaces = 0
+  
+  board.each do |index|
+    #if the spaces are occupied with X or O, add to returned counter
+    if board[turnCounter] == "X"
+      occupiedSpaces += 1 
+    elsif board[turnCounter] == "O"
+      occupiedSpaces += 1
+    end
+    turnCounter += 1
+  end
+  return occupiedSpaces
+end
+
+def current_player
+  
+end
